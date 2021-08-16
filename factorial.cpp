b7a3bb4c6af568cfac87be2ad5d6cf1fad6ea879
Priyam Seth <sethpriyam1@gmail.com>
2021-08-16 16:15:14 +0530

    Question 8 done

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,15 @@
 #include "functions.h"
+#include <iostream>
+
+using namespace std;
+
+
 
 int factorial(int n){
+    while (n < 0) {
+        cout << "\n A negative number found! Enter a positive number: ";
+        cin >> n;
+    }
     if(n!=1){
 	return(n * factorial(n-1));
     }
